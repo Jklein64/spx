2a73269a8bb231a474bef785ab1126970d4f7c33
Jklein64 <jklein.64@verizon.net>
2021-06-24 14:12:07 -0400

    Close window after 1 second

--- pages/1/[auto].js
+++ pages/1/[auto].js
@@ -9,6 +9,12 @@ const auto = () => {
 
     console.log('[info] redirecting user to url:', url)
     window.location = url
+
+    // hack to close the current window.  Won't work on all browsers, but works for chrome as of 6/24/2021
+    window.setTimeout(() => {
+        window.open('', '_parent', '').close();
+    }, 1000)
+
     return null
 }
 
